0d69df1555a5f07544df017d677a8302d09b8337
abeer609 <mdsfat609@gmail.com>
2023-08-06 09:06:45 +0600

    selection sort implemented

--- bubbleSort/app.js
+++ bubbleSort/app.js
@@ -21,37 +21,52 @@ class Container {
     this.delay = (ms) => new Promise((res) => setTimeout(res, ms));
   }
   draw() {
-    for (const i of this.arr) {
-      const b = new Box(i);
-      b.dom.style.height = `${b.value * 16}px`;
+    if (this.container.children) {
+      $(this.container).empty();
+      //   for (const child of this.container.children) {
+      //     child.remove();
+      //   }
+    }
+    for (let i = 0; i < this.arr.length; i++) {
+      const b = new Box(this.arr[i]);
       $(this.container).append(b.dom);
+      const max = Math.max(...this.arr);
+      console.log(max);
+      b.dom.style.height = `${b.value * (200 / max)}px`;
+      b.dom.style.left = `${i * ($(b.dom).width() + 8)}px`;
+      b.dom.style.bottom = `${0}px`;
+      b.pos = i * ($(b.dom).width() + 8);
       this.boxes.push(b);
     }
   }
 
   swap(idx1, idx2) {
     const left = this.boxes[idx1];
-    left.dom.classList.add("check");
     const right = this.boxes[idx2];
+    // before swap
     const temp = this.boxes[idx1];
     this.boxes[idx1] = this.boxes[idx2];
     this.boxes[idx2] = temp;
     // console.log(left);
-    left.pos += $(left.dom).outerWidth() + 8;
+    left.pos += (idx2 - idx1) * ($(left.dom).outerWidth() + 8);
     $(left.dom).animate({
       left: `${left.pos}px`,
     });
-    right.pos -= $(right.dom).outerWidth() + 8;
+    right.pos -= (idx2 - idx1) * ($(right.dom).outerWidth() + 8);
     $(right.dom).animate({
       left: `${right.pos}px`,
     });
+    //swap finished
+  }
+  check(idx1, idx2) {
+    $(this.boxes[idx1].dom).addClass("check");
+    $(this.boxes[idx2].dom).addClass("check");
   }
   x = 0;
   sort = async () => {
     for (let i = 0; i < this.arr.length; i++) {
       for (let j = 0; j < this.arr.length - i - 1; j++) {
-        $(this.boxes[j].dom).addClass("check");
-        $(this.boxes[j + 1].dom).addClass("check");
+        this.check(j, j + 1);
         if (this.arr[j + 1] < this.arr[j]) {
           await this.delay(500);
           this.swap(j, j + 1);
@@ -68,7 +83,7 @@ class Container {
     console.log("Finished");
   };
 }
-c = new Container(container, [1, 3, 10, 9, 8, 7, 6, 5, 4]);
+c = new Container(container, [3, 1, 10, 9, 8, 7, 6, 5, 4]);
 c.draw();
 $("#swap").click(() => {
   c.sort();
@@ -78,5 +93,65 @@ $("#reset").click(() => {
   $(".box").remove();
   c = new Container(container, [1, 3, 10, 9, 8, 7, 6, 5, 4]);
   //   c.boxes = [];
+  c.draw();
+});
+
+class SelectionSort extends Container {
+  sort = async () => {
+    for (let i = 0; i < this.arr.length; i++) {
+      let index = i;
+      console.log(this.arr[index]);
+      for (let j = i; j < this.arr.length; j++) {
+        if (this.arr[j] < this.arr[index]) {
+          index = j;
+        }
+      }
+      const temp = this.arr[i];
+      this.arr[i] = this.arr[index];
+      this.arr[index] = temp;
+      this.check(i, index);
+      await this.delay(500);
+      if (i != index) {
+        this.swap(i, index);
+        await this.delay(500);
+      }
+      $(this.boxes[i].dom).removeClass("check");
+      $(this.boxes[index].dom).removeClass("check");
+      console.log("sorted", this.arr[i]);
+      $(this.boxes[i].dom).addClass("sorted");
+      await this.delay(1000);
+    }
+  };
+}
+
+s = new SelectionSort(
+  document.querySelector(".boxes"),
+  [1, 3, 10, 9, 8, 7, 6, 5, 4]
+);
+
+s.draw();
+
+$("#swap2").click(() => {
+  s.sort();
+});
+
+$("#reset2").click(() => {
+  $(".container2 >.box").remove();
+  s = new SelectionSort(
+    document.querySelector(".boxes"),
+    [1, 3, 10, 9, 8, 7, 6, 5, 4]
+  );
+  //   c.boxes = [];
+  s.draw();
+});
+
+$(window).on("resize", function () {
+  //   s = new SelectionSort(
+  //     document.querySelector(".boxes"),
+  //     [1, 3, 10, 9, 8, 7, 6, 5, 4]
+  //   );
+  s.boxes = [];
+  s.draw();
+  c.boxes = [];
   c.draw();
 });
